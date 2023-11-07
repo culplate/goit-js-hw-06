352a6f910e7a2c7827c81a0d241ca50aedfb31c3
culplate <culplate@gmail.com>
2023-11-07 21:19:52 +0100

    Completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  // записую результат функції в змінну, бо якщо запускати функцію
+  // в body.style.backgroundColor, а потім span змінювати ось так -
+  // span.innerHTML = body.style.backgroundColor, то в span буде передаватись
+  // RGB значення, а не HEX
+  const hexColor = getRandomHexColor();
+  body.style.backgroundColor = hexColor;
+  span.innerHTML = hexColor;
+});
